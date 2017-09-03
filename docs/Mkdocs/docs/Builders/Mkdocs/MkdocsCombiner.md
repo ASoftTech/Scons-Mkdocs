af366372d235993c90bcadf10c6de15647e00971
grbd <garlicbready@googlemail.com>
2017-09-03 01:14:45 +0000

    updates to MkdocsCombiner

--- docs/Mkdocs/docs/Builders/Mkdocs/MkdocsCombiner.md
+++ docs/Mkdocs/docs/Builders/Mkdocs/MkdocsCombiner.md
@@ -7,9 +7,8 @@ Pandoc can then be later used to generate a pdf or epub file from this .pd expor
 The output file is basically a combination of all pages into a single markdown file.
 
 Note to get this to work you'll need to install from git since mkdocs-combiner isn't currently published on pypi.
-Also the below fork should have a couple of minor fixes for python3.
 ```
-pip install git+https://github.com/grbd/mkdocs-combine.git
+pip install git+https://github.com/twardoch/mkdocs-combine.git
 ```
 
 
@@ -36,12 +35,42 @@ env.Replace(Mkdocs_Pandoc_Width = 100)
 
 ## Available Options
 
+### File Options
+
 | env setting | Values | Description |
 |-------------|--------|-------------|
-| MkdocsCombiner | 'mkdocscombine' | Override the executable / path to use |
+| Mkdocs_Combine | 'mkdocscombine' | Override the executable / path to use |
 | Mkdocs_WorkingDir | Current Directory | Can be set to override the working directory where mkdocs will be run |
-| Mkdocs_Pandoc_Encoding | None (default) | Can be used to override the encoding for input files (default is utf-8) |
-| Mkdocs_Pandoc_ImageExt | None (default) | Extension to substitute image extensions by (default: no replacement) |
-| Mkdocs_Pandoc_Width | None (default) | Width of generated grid tables in characters (default: 100) |
-| Mkdocs_Pandoc_Exclude | None (default) | Include files to skip (default: none) |
-| Mkdocs_ExtraArgs | [] (default) | Additional options to pass to mkdocs as an array |
+| Mkdocs_Combine_Encoding | None (default) | Can be used to override the encoding for input files (default is utf-8) |
+| Mkdocs_Combine_Exclude | None (default) | Include files to skip (default: none) |
+| Mkdocs_Combine_OutputHtml | False (default), True | If to output a single html page instead of markdown |
+
+### Structure options
+
+| env setting | Values | Description |
+|-------------|--------|-------------|
+| Mkdocs_Combine_Meta | None (default), True, False | If to keep YAML metadata (default), False = strip YAML metadata |
+| Mkdocs_Combine_Titles | None (default), True, False | Add titles from mkdocs.yml to Markdown files (default), False = do not add titles to Markdown files |
+| Mkdocs_Combine_Uplevels | None (default), True, False | Increase ATX header levels in Markdown files (default), False = do not increase ATX header levels in Markdown files |
+
+### Table options
+
+| env setting | Values | Description |
+|-------------|--------|-------------|
+| Mkdocs_Combine_PandocTables | None (default), True, False | True = keep original Markdown tables (default), False = combine Markdown tables to Pandoc-style grid tables |
+| Mkdocs_Combine_TableWidth | None (default) | Width of generated grid tables in characters (default: 100) |
+
+### Link options
+
+| env setting | Values | Description |
+|-------------|--------|-------------|
+| Mkdocs_Combine_Refs | None (default), True, False | True = keep MkDocs-style cross-references, False = replace MkDocs-style cross-references by just their title (default) |
+| Mkdocs_Combine_Anchors | None (default), True, False | True = keep HTML anchor tags, False = strip out HTML anchor tags (default) |
+
+### Extra options
+
+| env setting | Values | Description |
+|-------------|--------|-------------|
+| Mkdocs_Combine_MathLatex | None (default), True, False | True = combine the \( \) Markdown math into LaTeX $$ inlines, False = keep \( \) Markdown math notation as is (default) |
+| Mkdocs_Combine_ImageExt | None (default) | Extension to substitute image extensions by (default: no replacement) |
+| Mkdocs_Combine_ExtraArgs | [] (default) | Additional options to pass to mkdocs as an array |
--- scons_tools_grbd/Tools/Docs/Mkdocs/MkdocsCombiner.py
+++ scons_tools_grbd/Tools/Docs/Mkdocs/MkdocsCombiner.py
@@ -3,11 +3,6 @@ MkdocsCombiner
   This tool uses mkdocscombiner to generate a pd file which can then be used with pandoc to generate a pdf or other forms of documentation
 """
 
-# TODO
-# 1. rename options from Pandoc to Combine
-# 2. Add additional options
-# 3. Update options in the docs
-
 import os, sys, os.path as path
 import SCons.Script
 from SCons.Environment import Environment
@@ -15,8 +10,8 @@ from SCons.Script import *
 
 
 def _detect(env):
-    if 'MkdocsCombiner' in env:
-        return env['MkdocsCombiner']
+    if 'Mkdocs_Combine' in env:
+        return env['Mkdocs_Combine']
     return env.Detect("mkdocscombine")
 
 def exists(env):
@@ -28,18 +23,49 @@ def generate(env):
     assert(exists(env))
     # Available Options - These override those within the yaml configuration file
     env.SetDefault(
+
+        # File options
+
         # Working directory is current directory (default)
         Mkdocs_WorkingDir = env.Dir('.'),
         # Set encoding for input files (default: utf-8)
-        Mkdocs_Pandoc_Encoding = None,
-        # Extension to substitute image extensions by (default: no replacement)
-        Mkdocs_Pandoc_ImageExt = None,
-        # Width of generated grid tables in characters (default: 100)
-        Mkdocs_Pandoc_Width = None,
+        Mkdocs_Combine_Encoding = None,
         # Include files to skip (default: none)
-        Mkdocs_Pandoc_Exclude = None,
+        Mkdocs_Combine_Exclude = None,
+        # If to output a single html page instead of markdown
+        Mkdocs_Combine_OutputHtml = False,
+
+        # Structure options
+
+        # If to keep YAML metadata (default), False = strip YAML metadata
+        Mkdocs_Combine_Meta = None,
+        # Add titles from mkdocs.yml to Markdown files (default), False = do not add titles to Markdown files
+        Mkdocs_Combine_Titles = None,
+        # Increase ATX header levels in Markdown files (default), False = do not increase ATX header levels in Markdown files
+        Mkdocs_Combine_Uplevels = None,
+
+        # Table options
+
+        # True = keep original Markdown tables (default), False = combine Markdown tables to Pandoc-style grid tables
+        Mkdocs_Combine_PandocTables = None
+        # Width of generated grid tables in characters (default: 100)
+        Mkdocs_Combine_TableWidth = None,
+
+        # Link options
+
+        # True = keep MkDocs-style cross-references, False = replace MkDocs-style cross-references by just their title (default)
+        Mkdocs_Combine_Refs = None
+        # True = keep HTML anchor tags, False = strip out HTML anchor tags (default)
+        Mkdocs_Combine_Anchors = None
+
+        # Extra options
+
+        # True = combine the \( \) Markdown math into LaTeX $$ inlines, False = keep \( \) Markdown math notation as is (default)
+        Mkdocs_Combine_MathLatex = None,
+        # Extension to substitute image extensions by (default: no replacement)
+        Mkdocs_Combine_ImageExt = None,
         # Additional Arguments
-        Mkdocs_ExtraArgs = [],
+        Mkdocs_Combine_ExtraArgs = [],
         )
 
     # Register the builder
@@ -69,24 +95,71 @@ def __MkdocsCombiner_func(target, source, env):
         if cfgfile:
             cmdopts.append('--config-file=' + cfgfile)
 
-        if outfile:
+        if env['Mkdocs_Combine_OutputHtml']:
+            cmdopts.append('--outhtml' + outfile)
+        else:
             cmdopts.append('--outfile=' + outfile)
 
-        if env['Mkdocs_Pandoc_Encoding']:
-            cmdopts.append('--encoding=' + str(env['Mkdocs_Pandoc_Encoding']))
+        # File options
 
-        if env['Mkdocs_Pandoc_ImageExt']:
-            cmdopts.append('--image-ext=' + str(env['Mkdocs_Pandoc_ImageExt']))
-
-        if env['Mkdocs_Pandoc_Width']:
-            cmdopts.append('--width=' + str(env['Mkdocs_Pandoc_Width']))
+        if env['Mkdocs_Combine_Encoding']:
+            cmdopts.append('--encoding=' + str(env['Mkdocs_Combine_Encoding']))
 
         # TODO parse list as input
-        if env['Mkdocs_Pandoc_Exclude']:
-            cmdopts.append('--exclude=' + str(env['Mkdocs_Pandoc_Exclude']))
+        if env['Mkdocs_Combine_Exclude']:
+            cmdopts.append('--exclude=' + str(env['Mkdocs_Combine_Exclude']))
 
-        cmdopts = cmdopts + env['Mkdocs_ExtraArgs']
+        # Structure options
 
-        print('Building MkDocs Documentation as Pandoc file:')
+        if env['Mkdocs_Combine_Meta'] == True:
+            cmdopts.append('--meta')
+        elif env['Mkdocs_Combine_Meta'] == False:
+            cmdopts.append('--no-meta')
+
+        if env['Mkdocs_Combine_Titles'] == True:
+            cmdopts.append('--titles')
+        elif env['Mkdocs_Combine_Titles'] == False:
+            cmdopts.append('--no-titles')
+
+        if env['Mkdocs_Combine_Uplevels'] == True:
+            cmdopts.append('--up-levels')
+        elif env['Mkdocs_Combine_Uplevels'] == False:
+            cmdopts.append('--keep-levels')
+
+        # Table options
+
+        if env['Mkdocs_Combine_PandocTables'] == True:
+            cmdopts.append('--grid-tables')
+        elif env['Mkdocs_Combine_PandocTables'] == False:
+            cmdopts.append('--tables')
+
+       if env['Mkdocs_Combine_TableWidth']:
+            cmdopts.append('--grid-width=' + str(env['Mkdocs_Combine_TableWidth']))
+
+        # Link options
+
+        if env['Mkdocs_Combine_Refs'] == True:
+            cmdopts.append('--refs')
+        elif env['Mkdocs_Combine_Refs'] == False:
+            cmdopts.append('--no-refs')
+
+        if env['Mkdocs_Combine_Anchors'] == True:
+            cmdopts.append('--anchors')
+        elif env['Mkdocs_Combine_Anchors'] == False:
+            cmdopts.append('--no-anchors')
+
+        # Extra options
+
+        if env['Mkdocs_Combine_MathLatex'] == True:
+            cmdopts.append('--latex')
+        elif env['Mkdocs_Combine_MathLatex'] == False:
+            cmdopts.append('--math')
+
+        if env['Mkdocs_Combine_ImageExt']:
+            cmdopts.append('--image-ext=' + str(env['Mkdocs_Combine_ImageExt']))
+
+        cmdopts = cmdopts + env['Mkdocs_Combine_ExtraArgs']
+
+        print('Building MkDocs Documentation as combined markdown file:')
         env.Execute(env.Action([cmdopts], chdir=env['Mkdocs_WorkingDir']))
         index = index + 1
